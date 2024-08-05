0da303e085cf111099775537642070d7b08d0b9c
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-08-05 17:31:02 -0300

    components
    Components were created to start the pages development

---
+++ src/Components/CreateItem/CreateItem.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CreateItem = () => {
+  return <div>CreateItem</div>;
+};
---
+++ src/Components/ToDoItem/ToDoItem.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ToDoItem = () => {
+  return <div>ToDoItem</div>;
+};
