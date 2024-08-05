d3d8a67cf17616e9ce5812a3421a09b3cbdade5c
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-08-05 19:14:12 -0300

    login-structure
    Login sctructure done, need to implement the routes and the styling

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import React, { ChangeEvent, useState } from "react";
+import "./Login.css";
+
+//Regex para validar o email
+const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 export const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  //Gerenciamento de login
+  const handleLogin = () => {
+    //Verificação regex para o email
+    if (!isEmail.test(email)) {
+      alert("Insira um email válido");
+      return;
+    }
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  //Gerenciamento de email
+  const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  //Gerenciamento de senha
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <Container>
+      <Box className="outter-box">
+        <Typography className="login-title" variant="h4" component="h1" mb={2}>
+          Login
+        </Typography>
+        <Box className="inner-box-inputs">
+          <TextField
+            required
+            label="Email"
+            variant="outlined"
+            value={email}
+            onChange={handleEmail}
+          />
+          <TextField
+            required
+            label="Password"
+            type="password"
+            variant="outlined"
+            value={password}
+            onChange={handlePassword}
+            autoComplete="current-password"
+          />
+        </Box>
+        <Box className="inner-box-buttons">
+          <Button variant="contained" onClick={handleLogin}>
+            Login
+          </Button>
+          <Button variant="text">Register now</Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
