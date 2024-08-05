e3572c11c7c3688e033ac9a2f3184a27eb80445d
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-08-05 20:33:02 -0300

    header-basic-structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { Login } from "./Components/Login/Login";
 import { Register } from "./Components/Register/Register";
+import { Header } from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Login />
       <Register />
     </div>
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,13 @@
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
+import "./Header.css";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container className="header">
+      <Typography className="header-title" variant="h4" component="h1" mb={2}>
+        Tude Diniz To-Do List
+      </Typography>
+    </Container>
+  );
 };
